403867e30836be339dd4354de34458eba8b13339
manojvenkat95 <smanojvenkat@gmail.com>
2025-01-28 09:24:19 -0500

    master second commit

--- src/Master/Test1.java
+++ src/Master/Test1.java
@@ -3,10 +3,10 @@ package Master;
 public class Test1 {
 
 	public static void main(String[] args) {
-		int a=100,b=200;
-		int sum=a+b;
+		int a = 100, b = 200;
+		int sum = a + b;
 		System.out.println(sum);
-
+		System.out.println("master");
 	}
 
 }
